c68956b305d012fc616a033fadf2510969a98ddf
twmeadow <twmeadow@uvm.edu>
2025-09-07 01:47:01 -0400

    first work sesh

--- readCSV.h
+++ readCSV.h
@@ -16,16 +16,17 @@ class csvData
     //Variable declaration
     private:
         string date, id;
-        int steps, minutes, heartBeatRate;
+        int steps, minutes, heartBeatRate, calories;
 
     public:
 
         //only one constructor with all fields initialized, because this is the only way I am going to call it
-        csvData(string id, string date, int steps, int minutes, int heartBeatRate)
+        csvData(string id, string date, int steps, int calories, int minutes, int heartBeatRate)
         {
             this->id = id;
             this->date = date;
             this->steps = steps;
+            this->calories = calories;
             this->minutes = minutes;
             this->heartBeatRate = heartBeatRate;
         }
@@ -42,12 +43,16 @@ class csvData
 
         int getHeartBeatRate() const {return heartBeatRate;}
 
+        int getCalories() const {return calories;}
+
 
         void setId(string id) {this->id = id;}
 
         void setDate(string date) {this->date = date;}
 
         void setSteps(int steps) {this->steps = steps;}
+
+        void setCalories(int calories) {this->calories = calories;}
 
         void setMinutes(int minutes) {this->minutes = minutes;}
 
@@ -59,7 +64,7 @@ class csvData
 };
 
 
-bool getDataFromFile(vector<csvData> &csvData, const string &fileName)
+bool getDataFromFile(vector<csvData>& csvDataV, const string &fileName)
 {
     //open file
     ifstream fileIn;
@@ -68,30 +73,71 @@ bool getDataFromFile(vector<csvData> &csvData, const string &fileName)
     if (fileIn)
     {
         //define variables for file
-        string id, date, header, endstr;
-        int steps, minutes, heartBeatRate;
+        string id, date, header, endstr, temp;
+        int steps, calories, minutes, heartBeatRate;
         char comma;
+        int count = 0;
 
         //read and check header
         getline(fileIn, header);
-        cout << header << endl;
+        //cout << header << endl;
 
         while (fileIn.peek() != EOF)
         {
             getline(fileIn, id, ',');
+
+
+
             getline(fileIn, date, ',');
 
-            if (fileIn.peek() != 0)
+
+
+            fileIn >> steps;
+            if (steps == 0)
             {
-                fileIn >> steps;
-            } else { steps = -1;}
+                steps = -1;
+            }
             fileIn >> comma;
 
-            if (fileIn.peek() != "Not-Given")
-            {
-                fileIn >> minutes;
-            }else{ minutes = -1;}
+
+            fileIn >> calories;
+            if (calories == 0) { calories = -1; }
             fileIn >> comma;
+
+
+
+            getline(fileIn, temp, ',');
+            if (temp != "Not-Given")
+            {
+
+                minutes = std::stoi(temp);
+            }else{ minutes = -1;}
+
+
+            //fileIn >> comma;
+
+
+            getline(fileIn, temp);
+            if (temp != "Not-Given")
+            {
+                heartBeatRate = std::stoi(temp);
+            }else{ heartBeatRate = -1;}
+
+
+
+
+            if (steps > 0 && calories > 0 && minutes > 0 && heartBeatRate > 0)
+            {
+                csvDataV.push_back(csvData(id, date, steps, calories, minutes, heartBeatRate));
+                cout << "id:" << id << endl;
+                cout << "Date:" << date << endl;
+                cout << "steps:" << steps << endl;
+                cout << "calories:" << calories << endl;
+                cout << "minutes:" <<minutes << endl;
+                cout << "heartBeatRate:" << heartBeatRate << endl;
+                cout << endl << "Count: " << count << endl << endl;
+                count = count + 1;
+            }
         }
 
 
